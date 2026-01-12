cf962401a8580df34d3b452e937e218b9d7ee7bc
nullzeiger <ivan.guerreschi.dev@gmail.com>
2026-01-12 20:42:16 +0100

    Add GitHub actions for build and release

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,4 +1,4 @@
-// Copyright 2025 Ivan Guerreschi. All rights reserved.
+// Copyright 2026 Ivan Guerreschi. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
